bfe1f7e8b77cfc57b4048529680d01b702aad365
Your Name~ <you@example.com>
2025-03-16 03:31:53 +0500

    ...

--- src/components/custom/WorldMapSection.js
+++ src/components/custom/WorldMapSection.js
@@ -1,6 +1,6 @@
 export default function WorldMapSection() {
   return (
-    <div className="bg-gradient-to-r from-amber-700 to-amber-500 min-h-[400px] flex flex-col items-center justify-center px-4 py-10 text-white">
+    <div className="bg-gradient-to-r from-black to-black/70 min-h-[400px] flex flex-col items-center justify-center px-4 py-10 text-[#c7a067]">
       <div className="container max-w-5xl mx-auto text-center">
         {/* Stats Row */}
         <div className="flex flex-wrap items-center justify-center gap-6 sm:gap-10 lg:gap-20 lg:mb-10">
@@ -13,7 +13,7 @@ export default function WorldMapSection() {
           {/* World Map */}
           <div className="flex  h-[150px] w-[300px] sm:h-[200px] sm:w-[400px]  lg:ml-[-50px]">
             <img
-              src="/images/world-map.gif"
+              src="/images/world-map2.gif"
               alt="World Map"
               className="opacity-80 h-full w-full"
             />
@@ -27,10 +27,14 @@ export default function WorldMapSection() {
         </div>
 
         {/* AI Scientists */}
-        <div className="flex items-center justify-center mt-6 sm:mt-10">
+        <div className="flex flex-col md:flex-row gap-7 items-center justify-center mt-6 sm:mt-10">
           <div className="flex flex-col items-center lg:ml-[100px]">
             <div className="text-4xl sm:text-5xl font-light">10+</div>
             <div className="text-sm mt-1">AI Scientists</div>
+          </div>
+          <div className="flex flex-col items-center lg:ml-[100px]">
+            <div className="text-4xl sm:text-5xl font-light">200+</div>
+            <div className="text-sm mt-1">Use Cases</div>
           </div>
         </div>
       </div>
